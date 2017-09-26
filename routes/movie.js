e9d4c4cd0e35841213531f0763462e9e6125670e
Jonny Hork <JonnyHork@gmail.com>
2017-09-26 14:40:42 -0600

    movie routes done

--- routes/movie.js
+++ routes/movie.js
@@ -4,7 +4,29 @@ const knex = require('../knex')
 
 // C
 router.post('/', (req, res, next) => {
-  res.send('post');
+  console.log('req.body is:', req.body);
+  console.log(req.body.year);
+
+  knex('movie')
+    .insert({
+      title: req.body.title,
+      year: req.body.year,
+      director_id: req.body.director_id
+    }) // add a returning '*' statement
+    .then((movie) => {
+      console.log('movie= ', movie);
+      let newMovie = {
+        title: movie[0].title,
+        year: movie[0].year,
+        director_id: movie[0].director_id
+      }
+
+      res.setHeader('Content-Type', 'application/json')
+      res.send(newMovie)
+
+    }).catch((err) => next(err))
+
+  // res.send('post');
 })
 // R
 router.get('/', (_req, res, next) => {
@@ -13,21 +35,85 @@ router.get('/', (_req, res, next) => {
     .then((movies) => {
       res.setHeader('Content-Type', 'application/json')
       res.send(movies)
-    })
+    }).catch((err) => next(err))
 
   // res.send('get all');
 })
 
 router.get('/:id', (req, res, next) => {
-  res.send('get one');
+
+  let id = Number(req.params.id)
+
+  knex('movie')
+    .select('id', 'title', 'year')
+    .where('id', id)
+    .then((movie) => {
+      // err handle if id doesnt exist
+      res.setHeader('Content-Type', 'application/json')
+      res.send(movie)
+    }).catch((err) => next(err))
+
+
+  // res.send('get one');
 })
 // U
 router.patch('/:id', (req, res, next) => {
-  res.send('patch');
+  console.log('req.body= ', req.body);
+  let id = Number(req.params.id)
+
+  // if (req.body === {}) {
+  //   return res.status(404).send('Can not update movie without info')
+  // }
+
+  knex('movie')
+
+    .update({
+      title: req.body.title,
+      year: req.body.year,
+      director_id: req.body.director_id
+    })
+    .where('id', id)
+    .then((movie) => {
+      console.log('movie', movie);
+
+      if (movie !== 1) {
+        return res.status(404).send('No movie was updated')
+      }
+
+      let updatedMovie = {
+        id: id,
+        title: req.body.title,
+        year: req.body.year,
+        director_id: req.body.director_id
+      }
+
+      res.setHeader('Content-Type', 'application/json')
+      res.send(updatedMovie)
+
+    })
+    .catch((err) => next(err))
+
+
+  // res.send('patch');
 })
 // D
 router.delete('/:id', (req, res, next) => {
-  res.send('delete');
+
+  let id = Number(req.params.id)
+
+  knex('movie')
+    .del()
+    .where('id', id)
+    .then((delMovie) => {
+
+      if (delMovie !== 1) {
+        return res.status(404).send('No movie was deleted')
+      }
+
+      res.status(200).send(`movie with the id: ${id} was deleted`)
+    })
+
+  // res.send('delete');
 })
 
 
