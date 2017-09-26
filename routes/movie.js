e6996b4d8acae2fe9204085c76600110618c3cd1
Jonny Hork <JonnyHork@gmail.com>
2017-09-26 12:11:52 -0600

    movie get all route set up

--- routes/movie.js
+++ routes/movie.js
@@ -1,7 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('../knex')
 
-/* GET home page. */
+// C
+router.post('/', (req, res, next) => {
+  res.send('post');
+})
+// R
+router.get('/', (_req, res, next) => {
+  knex('movie')
+    .select('id', 'title', 'year')
+    .then((movies) => {
+      res.setHeader('Content-Type', 'application/json')
+      res.send(movies)
+    })
+
+  // res.send('get all');
+})
+
+router.get('/:id', (req, res, next) => {
+  res.send('get one');
+})
+// U
+router.patch('/:id', (req, res, next) => {
+  res.send('patch');
+})
+// D
+router.delete('/:id', (req, res, next) => {
+  res.send('delete');
+})
+
+
+module.exports = router;
+
 
 
 module.exports = router;
